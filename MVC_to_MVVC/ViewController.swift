737d0be5c6ca5f97dd40d39a80a77f5480d2a418
Zaur Giyasov <zagiyas@mera.ru>
2018-09-12 11:52:23 +0300

    Initial Commit

---
+++ MVC_to_MVVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVC_to_MVVC
+//
+//  Created by Zaur Giyasov on 12/09/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
